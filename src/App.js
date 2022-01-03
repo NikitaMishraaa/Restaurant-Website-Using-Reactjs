bf84ea5a9da493fafbc22ab0e2f1469c9d77b32f
Nikita Mishra <86710416+NikitaMishraaa@users.noreply.github.com>
2022-01-03 14:35:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Restaurant from './Components/Restaurant';
+
+function App() {
+  return (
+    <>
+     <Restaurant/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MenuApi.jsx
@@ -0,0 +1,88 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi are fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used.  ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Pakora, also know as pakoda or pakodi, are the Indian version of a fritter - crispy batter coating a variety of fillings from paneer and chicken to chillies and, as here, potatoes. We're serving them here with a tangy green chutney to make a great starter to an Indian meal. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Corn is a tall annual cereal grass (Zea mays) that is widely grown for its large elongated ears of starchy seeds. The seeds, which are also known as corn, are used as food for humans and livestock and as a source of biofuel and can be processed into a wide range of useful chemicals."
+        
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "As a rich source of vitamins, minerals, and fiber, chickpeas may offer a variety of health benefits, such as aiding weight management, improving digestion, and reducing your risk of disease. Additionally, this legume is high in protein and makes an excellent replacement for meat in many vegetarian and vegan dishes. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot. ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Thali includes mutton soup, aromatic mutton biryani made with seeraga samba rice, chettinad egg masala, mutton sukka, tawa fried prawns and karandi omelette.Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.Lorem ipsum, or lipsum as it is sometimes known.",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Many Indian desserts are fried foods made with sugar, milk or condensed milk. ... In the eastern part of India, for example, most are based on milk products. Many are flavoured with almonds and pistachios, spiced with cardamon, nutmeg, cloves and black pepper, and decorated with nuts, or with gold or silver leaf."
+      
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma chawal is also healthy as it contains good quality of carbohydrates and proteins. With our Punjabi rajma chawal recipe, you can delight your friends and family! This popular dish is loved by people of all age groups. Rajma Chawal is perfect for a Sunday brunch or on a get-together. It is cooked by using easily available ingredients like rajma (kidney beans) rice, onions, tomatoes and a selection of spices. So go ahead and .",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "A samosa is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, peas, chicken and/or other meats. ... Samosas are a popular entrée, appetizer, or snack in the local cuisines of South Asia, the Middle East, Central Asia, East Africa, Pakistan and other regions. ",
+    },
+  ];
+  
+  export default Menu;
